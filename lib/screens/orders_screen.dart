ce4a71bba66bd83ab6ccf199d589fa2958109055
Braden Donaldson <Braden@Bradens-MBPro.local>
2020-07-09 13:45:37 -0700

    added project

---
+++ lib/screens/orders_screen.dart
@@ -0,0 +1,55 @@
+import "package:flutter/material.dart";
+import 'package:provider/provider.dart';
+
+import '../providers/orders.dart' show Orders;
+import '../widgets/order_item.dart';
+import '../widgets/app_drawer.dart';
+
+class OrdersScreen extends StatelessWidget {
+  static const routeName = '/orders';
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    print('building orders');
+    // final orderData = Provider.of<Orders>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Orders'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.home),
+            onPressed: () {
+              Navigator.of(context).pushReplacementNamed('/');
+            },
+          ),
+        ],
+      ),
+      drawer: AppDrawer(),
+      body: FutureBuilder(
+        future: Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+        builder: (ctx, dataSnapshot) {
+          if (dataSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else {
+            if (dataSnapshot.error != null) {
+              //...
+              //Do error handling stuff
+              return Center(
+                child: Text('An error occured!'),
+              );
+            } else {
+              return Consumer<Orders>(
+                builder: (ctx, orderData, child) => ListView.builder(
+                  itemCount: orderData.orders.length,
+                  itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
+                ),
+              );
+            }
+          }
+        },
+      ),
+    );
+  }
+}
